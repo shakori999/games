4ac8204072a85d01b2a99c25d04b830d36f561f3
unknown <shakori999@gmail.com>
2021-06-20 15:51:06 +0300

    make the bullets move

--- Asteroids/main.lua
+++ Asteroids/main.lua
@@ -1,11 +1,16 @@
 love.load = function()
-    shipX = 800 / 2
-    shipY = 600 / 2
+    arenaWidth = 800
+    arenaHeight = 600
+
+    shipX = arenaWidth / 2
+    shipY = arenaHeight / 2
     shipAngle = 0
     shipSpeedX = 0
     shipspeedY = 0
+    shipRadius = 30
     speed = 100
 
+    bullets = {}
 end
 
 love.update = function(dt)
@@ -24,21 +29,47 @@ love.update = function(dt)
         shipSpeedX = shipSpeedX - math.cos(shipAngle) * speed * dt
         shipspeedY = shipspeedY - math.sin(shipAngle) * speed * dt
     end
+    shipX = (shipX + shipSpeedX * dt) % arenaWidth
+    shipY = (shipY + shipspeedY * dt) % arenaHeight
 
-    shipX = shipX + shipSpeedX * dt
-    shipY = shipY + shipspeedY * dt
+    for bulletIndex, bullet in ipairs(bullets) do
+        local bulletspeed = 500
+        bullet.x = (bullet.x + math.cos(bullet.angle) * bulletspeed * dt) % arenaWidth
+        bullet.y = (bullet.y + math.sin(bullet.angle) * bulletspeed * dt) % arenaWidth
+    end
+        
 end
 
 love.draw = function()
-    love.graphics.setColor(0, 0, 1)
-    love.graphics.circle('fill', shipX , shipY , 30)
+    for y = -1, 1 do
+        for x = -1, 1 do
+            love.graphics.origin()
+            love.graphics.translate(x * arenaWidth, y * arenaHeight)
 
-    local shipCircleDistance = 20
-    love.graphics.setColor(0, 1, 1)
-    love.graphics.circle("fill",
-        shipX + math.cos(shipAngle) * shipCircleDistance,
-        shipY + math.sin(shipAngle) * shipCircleDistance,
-        5 
-    )
-    love.graphics.print(shipAngle)
+            love.graphics.setColor(0, 0, 1)
+            love.graphics.circle('fill', shipX , shipY , shipRadius)
+
+            local shipCircleDistance = 20
+            love.graphics.setColor(0, 1, 1)
+            love.graphics.circle("fill",
+                shipX + math.cos(shipAngle) * shipCircleDistance,
+                shipY + math.sin(shipAngle) * shipCircleDistance,
+                5 
+            )
+            for bulletIndex, bullet in ipairs(bullets) do
+                love.graphics.setColor(0, 1, 0)
+                love.graphics.circle('fill', bullet.x, bullet.y, 5)
+            end
+        end
+    end
+end
+
+love.keypressed = function(key)
+    if key == 's' then
+        table.insert(bullets, {
+            x = shipX + math.cos(shipAngle) * shipRadius ,
+            y = shipY + math.sin(shipAngle) * shipRadius,
+            angle = shipAngle,
+        })
+    end
 end
